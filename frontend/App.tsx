bc898520a0d1342269bdf288d72da81de075f2df
AramArDev <arakelyan.aram.dev@gmail.com>
2022-01-06 05:21:22 +0100

    feat: 0

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,16 +1,19 @@
-import { HARDHAT_PORT, HARDHAT_PRIVATE_KEY } from '@env';
-import { useWalletConnect } from '@walletconnect/react-native-dapp';
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import localhost from 'react-native-localhost';
-import Web3 from 'web3';
+/* eslint-disable react-native/no-inline-styles */
+import { INFURA_KEY, HARDHAT_PORT, HARDHAT_PRIVATE_KEY } from "@env";
+import { useWalletConnect } from "@walletconnect/react-native-dapp";
+import React from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+import Web3 from "web3";
 
-import Hello from '../artifacts/contracts/Hello.sol/Hello.json';
+import Homepage from "./Homepage";
+
+// import LeafDapp from "../artifacts/contracts/LeafDapp.sol/LeafDapp.json";
+
 
 const styles = StyleSheet.create({
-  center: { alignItems: 'center', justifyContent: 'center' },
+  center: { alignItems: "center", justifyContent: "center" },
   // eslint-disable-next-line react-native/no-color-literals
-  white: { backgroundColor: 'white' },
+  white: { backgroundColor: "white" },
 });
 
 const shouldDeployContract = async (web3, abi, data, from: string) => {
@@ -24,63 +27,52 @@ const shouldDeployContract = async (web3, abi, data, from: string) => {
 
 export default function App(): JSX.Element {
   const connector = useWalletConnect();
-  const [message, setMessage] = React.useState<string>('Loading...');
   const web3 = React.useMemo(
-    () => new Web3(new Web3.providers.HttpProvider(`http://${localhost}:${HARDHAT_PORT}`)),
-    [HARDHAT_PORT]
+    () =>
+      new Web3(
+        new Web3.providers.HttpProvider(
+          `https://ropsten.infura.io/v3/${INFURA_KEY}`
+        )
+      ),
+    [INFURA_KEY]
   );
+  const [account, setAccount] = React.useState("");
+ 
+
+  const connectWallet = React.useCallback(() => {   
+    const connect = connector.connect();
+    setAccount(connector.accounts[0]);
+    return connect;
+  }, [connector, account]);
+
   React.useEffect(() => {
     void (async () => {
-      const { address } = await web3.eth.accounts.privateKeyToAccount(HARDHAT_PRIVATE_KEY);
-      const contract = await shouldDeployContract(
-        web3,
-        Hello.abi,
-        Hello.bytecode,
-        address
-      );
-      setMessage(await contract.methods.sayHello('React Native').call());
+      if (connector.connected) {
+        setAccount(connector.accounts[0]);
+      }
     })();
-  }, [web3, shouldDeployContract, setMessage, HARDHAT_PRIVATE_KEY]);
-  const connectWallet = React.useCallback(() => {
-    return connector.connect();
-  }, [connector]);
-  const signTransaction = React.useCallback(async () => {
-    try {
-       await connector.signTransaction({
-        data: '0x',
-        from: '0xbc28Ea04101F03aA7a94C1379bc3AB32E65e62d3',
-        gas: '0x9c40',
-        gasPrice: '0x02540be400',
-        nonce: '0x0114',
-        to: '0x89D24A7b4cCB1b6fAA2625Fe562bDd9A23260359',
-        value: '0x00',
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  }, [connector]);
-  const killSession = React.useCallback(() => {
-    return connector.killSession();
-  }, [connector]);
+  }, [account]);
+
   return (
-    <View style={[StyleSheet.absoluteFill, styles.center, styles.white]}>
-      <Text >LEAF app</Text>
-      <Text testID="tid-message">{message}</Text>
-      {!connector.connected && (
-        <TouchableOpacity onPress={connectWallet}>
-          <Text>Connect a Wallet</Text>
-        </TouchableOpacity>
-      )}
-      {!!connector.connected && (
-        <>
-          <TouchableOpacity onPress={signTransaction}>
-            <Text>Sign a Transaction</Text>
-          </TouchableOpacity>
-          <TouchableOpacity onPress={killSession}>
-            <Text>Kill Session</Text>
-          </TouchableOpacity>
-        </>
-      )}
+
+    <View>
+      {connector.connected && <Homepage parentToChild = { connector }/>}
+
+      {!connector.connected &&
+        <View style={[styles.center, styles.white]}>
+          <Text></Text>
+          <Text></Text>
+          <Text></Text>
+          <Text style={{ fontSize: 24 }}>Play 2 Plant</Text>
+          <Text>Fais du sport, plante des arbres,</Text>
+          <Text>et gagne de la crypto.</Text>
+          <Image source={require('./imgs/tree333-1.png')} />
+          <Button
+            title="Connect"
+            onPress={() => connectWallet()}
+          />
+        </View>
+      }
     </View>
   );
 }
